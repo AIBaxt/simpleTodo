1331fbb592b745111b951e437d35f11b1741c110
Andrew <andrewbmi2@gmail.com>
2021-05-05 20:46:08 -0400

    Updated styling

--- src/Components/todo/AddTodo.js
+++ src/Components/todo/AddTodo.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { TextField, Paper } from '@material-ui/core';
+import { TextField, Paper, Button, Toolbar } from '@material-ui/core';
 
 function AddTodo() {
-    const textStyle = { width: 400 }
+    const textStyle = { width: "93%", padding: 0 }
+    const buttonStyle = { margin: "10px 0 0 0", padding: "10px 40px 10px 40px" };
     return (
-        <TextField id="title" label="Todo Title" style={textStyle} ></TextField>
+        <div>
+            <TextField id="title" label="Todo Title" style={textStyle} ></TextField>
+            <Button variant="contained" color="primary" style={buttonStyle}>Add Todo</Button>
+        </div>
     );
 };
 
